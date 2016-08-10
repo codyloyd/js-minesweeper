fa4a811a5c86844888240683fbdb2f4bd94d9b82
Cody Loyd <codyloyd@gmail.com>
2016-08-09 19:43:26 -0500

    generates board and hints

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
   var gameArray = createGameArray(9)
+  generateMines(15,gameArray)
+  generateHints(gameArray)
   renderGrid(gameArray)
 })
+
 
 function createGameArray(size){
   var array = []
@@ -14,7 +17,7 @@ function createGameArray(size){
 function createArrayRow(size){
   var array = []
   for (var i = 0; i < size; i++) {
-    array.push(" ")
+    array.push(0)
   }
   return array
 }
@@ -23,6 +26,32 @@ function renderGrid(array){
   for (var i = 0; i < array.length; i++) {
     for (var j = 0; j < array[i].length; j++) {
       $(".gamecontainer").append('<div class="square">'+array[i][j]+'</div>')
+    }
+  }
+}
+
+function generateMines(numberOfMines,ary){
+  function rand() {
+    return Math.floor(Math.random()*ary.length)
+  }
+  for (var i = 0; i < numberOfMines; i++) {
+    ary[rand()][rand()] = "X"
+  }
+}
+
+function generateHints(gameArray){
+  var neighbors = [[-1,0],[0,-1],[1,0],[0,1],[1,1],[1,-1],[-1,1],[-1,-1]]
+  for (var i = 0; i < gameArray.length; i++) {
+    for (var j = 0; j < gameArray[i].length; j++) {
+      if (gameArray[i][j] == "X") {
+        for (var x = 0; x < neighbors.length; x++) {
+          if (i + neighbors[x][0] >= 0 && i + neighbors[x][0] <= 8 && j + neighbors[x][1] >= 0 && j + neighbors[x][1] <= 8 ) {
+            if(gameArray[i +neighbors[x][0]][j +neighbors[x][1]] != "X"){
+              gameArray[i +neighbors[x][0]][j +neighbors[x][1]] += 1
+            }
+          }
+        }
+      }
     }
   }
 }
